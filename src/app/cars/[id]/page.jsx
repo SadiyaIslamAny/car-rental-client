7fc1187f7d531f98598c5ba72474b0b86b84947a
SADIA <you@example.com>
2026-06-18 16:22:03 +0600

    implement car update modal with PATCH API integration

--- src/app/cars/[id]/page.jsx
+++ src/app/cars/[id]/page.jsx
@@ -1,3 +1,4 @@
+import EditModal from '@/components/EditModal';
 import Image from 'next/image';
 import React from 'react';
 
@@ -5,56 +6,62 @@ const carsDetailsPage = async ({ params }) => {
     const { id } = await params;
     // console.log(id)
 
-    const res = fetch(`http://localhost:5000/car/${id}`)
-    const car = await (await res).json()
-    // console.log(car)
+    const res = await fetch(`http://localhost:5000/car/${id}`)
+    const car = await res.json()
+    console.log(car)
     const { imageUrl, carName, dailyRentPrice, availability, description } = car;
     return (
+        <div className='max-w-3xl mx-auto mt-4'>
+            <div className='flex justify-end items-center gap-4 mt-5 mb-3'>
+                <EditModal car={car} />
 
-        <div className="max-w-3xl mx-auto space-y-8 border mt-8 rounded-2xl">
-            <div className="rounded-xl overflow-hidden shadow-lg">
-                <Image
-                    src={imageUrl}
-                    alt={carName}
-                    width={800}
-                    height={400}
-                    className=" object-cover mx-auto"
-                />
-            </div>
-
-            <div className="space-y-2 px-4 mb-5">
-
-
-                <span
-                    className={`inline-block px-4 py-1 rounded-full text-sm font-medium ${availability === "Available"
-                        ? "bg-green-100 text-green-700"
-                        : "bg-red-100 text-red-600"
-                        }`}
-                >
-                    {availability}
-                </span>
-                <div className='flex justify-between items-center'>
-                    <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800">
-                        {carName}
-                    </h1>
-
-                    <h2 className="text-xl sm:text-2xl font-semibold text-[#FF4C31]">
-                        ${dailyRentPrice}{" "}
-                    </h2>
-                </div>
-
-                <div className="pt-4">
-                    <h3 className="text-lg font-semibold text-gray-800 mb-2">
-                        Description:
-                    </h3>
-
-                    <p className="text-gray-600 leading-relaxed">
-                        {description}
-                    </p>
-                </div>
 
             </div>
+            <div className=" border mt-8 rounded-2xl space-y-8">
+                <div className="rounded-xl overflow-hidden shadow-lg">
+                    <Image
+                        src={imageUrl}
+                        alt={carName}
+                        width={800}
+                        height={400}
+                        className=" object-cover mx-auto"
+                    />
+                </div>
 
+                <div className="space-y-2 px-4 mb-5">
+
+
+                    <span
+                        className={`inline-block px-4 py-1 rounded-full text-sm font-medium ${availability === "Available"
+                            ? "bg-green-100 text-green-700"
+                            : "bg-red-100 text-red-600"
+                            }`}
+                    >
+                        {availability}
+                    </span>
+                    <div className='flex justify-between items-center'>
+                        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800">
+                            {carName}
+                        </h1>
+
+                        <h2 className="text-xl sm:text-2xl font-semibold text-[#FF4C31]">
+                            ${dailyRentPrice}{" "}
+                        </h2>
+                    </div>
+
+                    <div className="pt-4">
+                        <h3 className="text-lg font-semibold text-gray-800 mb-2">
+                            Description:
+                        </h3>
+
+                        <p className="text-gray-600 leading-relaxed">
+                            {description}
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     );
 };
---
+++ src/components/EditModal.jsx
@@ -0,0 +1,235 @@
+"use client";
+import { Button, FieldError, Input, Label, ListBox, Modal, Surface, TextArea, TextField, Select } from "@heroui/react";
+import { BiEdit } from "react-icons/bi";
+
+
+const EditModal =({ car }) => {
+  
+const { _id, imageUrl, carName, dailyRentPrice, carType, seatCapacity, pickupLocation, availability,description } = car;
+
+
+        const onSubmit = async(e) =>{
+          e.preventDefault()
+          const formData = new FormData(e.currentTarget)
+          const car = Object.fromEntries(formData.entries())
+          console.log(car)
+
+        const res = await fetch(`http://localhost:5000/car/${_id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(car)
+        })
+
+        const data = await res.json()
+        console.log(data)
+    }
+    return (
+
+        <Modal>
+  <Button variant="outline" className="rounded-none">
+    <BiEdit /> Update
+  </Button>
+
+  <Modal.Backdrop>
+    <Modal.Container placement="auto">
+      <Modal.Dialog className="sm:max-w-xl">
+        <Modal.CloseTrigger />
+
+        <Modal.Header>
+          <Modal.Heading>Edit Car</Modal.Heading>
+        </Modal.Header>
+
+        <Modal.Body className="p-6">
+          <Surface variant="default">
+            <form
+              onSubmit={onSubmit}
+              className="max-w-5xl mx-auto bg-white shadow-xl rounded-3xl p-6 md:p-10 space-y-8"
+            >
+              <div className="text-center">
+                <h2 className="text-3xl font-bold text-[#FF4C31]">
+                  Update Car
+                </h2>
+
+                <p className="text-gray-500 mt-2">
+                  Update your car information.
+                </p>
+              </div>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                <TextField name="carName" defaultValue={carName} isRequired>
+                  <Label className="font-semibold mb-2">
+                    Car Name
+                  </Label>
+                  <Input
+                    placeholder="Toyota Corolla"
+                    className="rounded-xl h-12"
+                  />
+                  <FieldError />
+                </TextField>
+
+                <TextField
+                  name="dailyRentPrice"
+                  type="number"
+                  defaultValue={dailyRentPrice}
+                  isRequired
+                >
+                  <Label className="font-semibold mb-2">
+                    Daily Rent Price ($)
+                  </Label>
+                  <Input
+                    placeholder="80"
+                    className="rounded-xl h-12"
+                  />
+                  <FieldError />
+                </TextField>
+
+            
+                <Select
+                  name="carType"
+                  defaultSelectedKeys={[carType]}
+                  isRequired
+                  className="w-full"
+                >
+                  <Label className="font-semibold mb-2">
+                    Car Type
+                  </Label>
+
+                  <Select.Trigger className="rounded-xl h-12">
+                    <Select.Value />
+                    <Select.Indicator />
+                  </Select.Trigger>
+
+                  <Select.Popover>
+                    <ListBox>
+                      <ListBox.Item id="SUV">SUV</ListBox.Item>
+                      <ListBox.Item id="Sedan">Sedan</ListBox.Item>
+                      <ListBox.Item id="Hatchback">Hatchback</ListBox.Item>
+                      <ListBox.Item id="Coupe">Coupe</ListBox.Item>
+                      <ListBox.Item id="Convertible">
+                        Convertible
+                      </ListBox.Item>
+                      <ListBox.Item id="Luxury">Luxury</ListBox.Item>
+                    </ListBox>
+                  </Select.Popover>
+                </Select>
+                <TextField
+                  name="seatCapacity"
+                  type="number"
+                  defaultValue={seatCapacity}
+                  isRequired
+                >
+                  <Label className="font-semibold mb-2">
+                    Seat Capacity
+                  </Label>
+
+                  <Input
+                    placeholder="5"
+                    className="rounded-xl h-12"
+                  />
+                  <FieldError />
+                </TextField>
+                <TextField
+                  name="pickupLocation"
+                  defaultValue={pickupLocation}
+                  isRequired
+                >
+                  <Label className="font-semibold mb-2">
+                    Pickup Location
+                  </Label>
+
+                  <Input
+                    placeholder="Dhaka Airport"
+                    className="rounded-xl h-12"
+                  />
+                  <FieldError />
+                </TextField>
+                <Select
+                  name="availability"
+                  defaultSelectedKeys={[availability]}
+                  isRequired
+                  className="w-full"
+                >
+                  <Label className="font-semibold mb-2">
+                    Availability Status
+                  </Label>
+
+                  <Select.Trigger className="rounded-xl h-12">
+                    <Select.Value />
+                    <Select.Indicator />
+                  </Select.Trigger>
+
+                  <Select.Popover>
+                    <ListBox>
+                      <ListBox.Item id="Available">
+                        Available
+                      </ListBox.Item>
+
+                      <ListBox.Item id="Unavailable">
+                        Unavailable
+                      </ListBox.Item>
+                    </ListBox>
+                  </Select.Popover>
+                </Select>
+
+                <div className="md:col-span-2">
+                  <TextField
+                    name="imageUrl"
+                    defaultValue={imageUrl}
+                    isRequired
+                  >
+                    <Label className="font-semibold mb-2">
+                      Image URL
+                    </Label>
+
+                    <Input
+                      type="url"
+                      placeholder="https://example.com/car.jpg"
+                      className="rounded-xl h-12"
+                    />
+
+                    <FieldError />
+                  </TextField>
+                </div>
+
+                <div className="md:col-span-2">
+                  <TextField
+                    name="description"
+                    defaultValue={description}
+                    isRequired
+                  >
+                    <Label className="font-semibold mb-2">
+                      Description
+                    </Label>
+
+                    <TextArea
+                      rows={5}
+                      placeholder="Write car details..."
+                      className="rounded-2xl"
+                    />
+
+                    <FieldError />
+                  </TextField>
+                </div>
+
+              </div>
+
+              <Button
+                type="submit"
+                className="w-full h-14 rounded-xl bg-[#FF4C31] text-white text-lg font-semibold"
+              >
+                Update Car
+              </Button>
+            </form>
+          </Surface>
+        </Modal.Body>
+      </Modal.Dialog>
+    </Modal.Container>
+  </Modal.Backdrop>
+</Modal>
+    );
+}
+
+export default EditModal;
